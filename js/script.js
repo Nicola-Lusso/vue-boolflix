56517f5f599106a85df938182285926be0bf138e
Nicola <nicolusso@gmail.com>
2021-02-12 13:57:32 +0100

    serietv+film complete

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ var app = new Vue ({
     lang: 'it-IT',
     query: "",
     films: [],
+    seriesTv: [],
+    flag: ['it', 'en', 'es', 'ja', 'ru', 'de', 'fr'],
 
   },
 
   methods: {
-    searchMovie() {
+    searchMovieTV() {
       axios
       .get("https://api.themoviedb.org/3/search/movie", {
         params: {
@@ -20,6 +22,20 @@ var app = new Vue ({
       .then((result) => {
         this.films = result.data.results;
         console.log(result.data.results);
+      })
+      .catch((error) => alert('Errore'));
+      axios
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params:{
+          api_key: '5b66b414fb41071efc4e6b12f91dd180',
+          language: this.lang,
+          query: this.query
+        }
+      })
+      .then((result) =>{
+        this.films = result.data.results;
+        console.log(result.data.results);
+
       })
       .catch((error) => alert('Errore'));
     }
